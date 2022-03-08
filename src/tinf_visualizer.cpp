5bf812ac41403f35f3ff9718dee86e152fe55951
William T. Jones <w.t.jones@nasa.gov>
2022-03-08 17:43:38 -0500

    o Implement tinf_visualization with Kombyne object

--- src/tinf_visualizer.cpp
+++ src/tinf_visualizer.cpp
@@ -1,20 +1,60 @@
+#include <iostream>
+
 #include "tinf_visualizer.h"
+#include "Kombyne.h"
+
+#define TINF_CHECK_SUCCESS(error, msg) ({ \
+  if( TINF_SUCCESS == error ) { \
+    std::stringstream ss; \
+    ss << error; \
+    std::string message = std::string(msg) + ": " + ss.str(); \
+    std::cerr << message << std::endl; \
+  } \
+})
 
 int32_t
-tinf_visualizer_create(void **vis, void *problem, void *mesh,
-                       void *solution, void *iris_comm)
+tinf_visualizer_create(void **visual, void *problem, void *mesh,
+                       void *solution, void *comm)
 {
+  try {
+    int32_t error;
+    int32_t nproc = tinf_iris_number_of_processes(comm, &error);
+    TINF_CHECK_STATUS(error, "Could not determine the number of processors");
+
+    VisKombyne::Kombyne* vis = new VisKombyne::Kombyne(problem, mesh, solution,
+                                                       comm, nproc);
+
+    *visual = (void*)vis;
+
+    return TINF_SUCCESS;
+  } catch( std::runtime_exception& e) {
+    std::cerr << e.what() << std::endl;
+  }
+
   return TINF_FAILURE;
 }
 
 int32_t
-tinf_visualizer_destroy(void **vis)
+tinf_visualizer_destroy(void **visual)
 {
-  return TINF_FAILURE;
+  VisKombyne::Kombyne* vis = (VisKombyne::Kombyne*)(*visual);
+  delete vis;
+
+  return TINF_SUCCESS;
 }
 
 int32_t
-tinf_visualizer(void *vis, _BOOL_ final_call)
+tinf_visualizer(void *visual, _BOOL_ final_call)
 {
+  try {
+    VisKombyne::Kombyne* vis = (VisKombyne::Kombyne*)(visual);
+
+    vis.execute();
+
+    return TINF_SUCCESS;
+  } catch( std::runtime_exception& e) {
+    std::cerr << e.what() << std::endl;
+  }
+
   return TINF_FAILURE;
 }
